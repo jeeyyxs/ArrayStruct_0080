cc63f75424db35766a307fc986c031a41c6b4e13
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-04-07 15:16:06 +0700

    3

--- Struct.cpp
+++ Struct.cpp
@@ -5,4 +5,19 @@ struct orang{
     string nama;
     string alamat;
     int umur;
+};
+
+int main ()
+{
+    //Deklarasi obyek dari struct orang
+    orang mhs;
+    //isi obyek
+    cout << "Nama Mahasiswa:";
+    cin >> mhs.nama;
+    cout << "Alamat Asal:";
+    cin >> mhs.alamat;
+    cout << "umur:";
+    cin >> mhs.umur;
+    cout << endl;
+    
 }
