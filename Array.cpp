5669cadcfbbe50144981084800572a233a128dea
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-04-07 15:01:36 +0700

    2

--- Array.cpp
+++ Array.cpp
@@ -10,5 +10,26 @@ int main(){
     //cara membaca array dataku
     cout << "Data ke -3 : " << dataku[2]<<endl;
 
-    
+    //Operasi Matematika
+    x = dataku[2]*3;
+    //cara menulis atau mengisi array
+    dataku[2] = 600;
+    cout << "Data ke - 3 yang baru : " << dataku[2]<<endl;
+
+    //mengisi array datamu satu per satu
+    cout << "Data Index ke - 0 : ";
+    cin >> datamu[0];
+    cout << "Data Index ke - 1 :";
+    cin >> datamu[1];
+    cout << "Data Index ke - 2 :";
+    cin >> datamu[2];
+    cout << "Data Index ke - 3 :";
+    cin >> datamu[3];
+    cout << "Data Index ke - 4 :";
+    cin >> datamu[4];
+
+    cout << endl;
+
+
+
 }
