bc1a391c231ccae8287981184f4290d390e7df49
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-04-07 14:54:31 +0700

    1

--- Array.cpp
+++ Array.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int main(){
+    //Deklarasi Array dengan inisialisasi
+    int dataku[5] = {10,20,30,40,50};
+    //deklarasi tanpa inisialisasi
+    int datamu[5];
+    int x;
+    //cara membaca array dataku
+    cout << "Data ke -3 : " << dataku[2]<<endl;
+
+    
+}
